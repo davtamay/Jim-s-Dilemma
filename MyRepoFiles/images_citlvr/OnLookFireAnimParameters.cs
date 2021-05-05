a6d770e8dbb8932607fb01915dd10e32da84ca68
davtamay <davtamay@gmail.com>
2021-05-05 17:47:29 -0500

    - updated readme

---
+++ MyRepoFiles/images_citlvr/OnLookFireAnimParameters.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+//using UnityEngine.EventSystems;
+//[RequireComponent(typeof(Collider))]
+public class OnLookFireAnimParameters : MonoBehaviour
+{
+
+
+	[SerializeField]private string nameOfBool;
+	private int boolHash;
+
+	[SerializeField]private float minAngleFromDown;
+	[SerializeField]private float maxAngleFromDown;
+
+	private Collider thisCollider;
+	private Transform camTransform;
+
+	[Header("Events")]
+	[SerializeField]private UnityEvent onSee;
+	[SerializeField]private UnityEvent onUnSee;
+//	[SerializeField]private UnityEvent onStart;
+
+	[Header("References")]
+	[SerializeField]private BoolVariable isStressMenuActive;
+	[SerializeField]private Animator animatorToTrigger;
+
+	void Start () {
+		
+		//onStart.Invoke();
+		boolHash = Animator.StringToHash (nameOfBool);
+		//thisCollider = GetComponent<Collider> ();
+		camTransform = Camera.main.transform;
+		//animatorToTrigger.SetBool (nameOfBool, true);
+		//animatorToTrigger.SetBool (nameOfBool, false);
+	}
+
+//	public void OnPointerEnter(PointerEventData eventData){
+//		
+//		animatorToTrigger.SetBool (nameOfBool, true);
+//		onSee.Invoke ();
+//	}
+//	public void OnPointerExit(PointerEventData eventData){
+//		animatorToTrigger.SetBool (nameOfBool, false);
+//		onUnSee.Invoke ();
+//	}
+	void Update () {
+
+		if (isStressMenuActive.isOn){
+			animatorToTrigger.SetBool (boolHash, true);
+			return;
+		}
+			
+
+		if (maxAngleFromDown > CameraAngleFromDown () && CameraAngleFromDown () > minAngleFromDown) {
+			onSee.Invoke ();
+			animatorToTrigger.SetBool (boolHash, true);
+
+		} else {
+			
+			animatorToTrigger.SetBool (boolHash, false);
+			onUnSee.Invoke ();
+		}
+//		RaycastHit hit;
+//		Ray ray = new Ray (camTransform.position, camTransform.rotation * Vector3.forward);
+//
+//		if (thisCollider.Raycast (ray, out hit, 50f)) {
+//			animatorToTrigger.SetBool (nameOfBool, true);
+//			onSee.Invoke ();
+//		} else {
+//			
+//			animatorToTrigger.SetBool (nameOfBool, false);
+//			onUnSee.Invoke ();
+//		}
+//		
+
+}
+	private float CameraAngleFromDown(){
+		return Vector3.Angle (Vector3.down, camTransform.rotation * Vector3.forward);}
+}
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Jim-s-Dilemma
+# Jim's-Dilemma
+
+![JimsDilemma](MyRepoFiles/gameWalkthrough.gif)
+
+## Goal 
+Showcase the Center for Innovation in Teaching and Learning VR site in a creative way
+
+## Type  
+Showcase
+
+## Platform
+Oculus Quest
+
+## Working state 
+Baseline application to show and receive appropriate data is done. 
+
+## Mechanics  
+Shine light on creatures trying to get the vr headsets in the room to scare them before you run out of them.
+
+## Story 
+The center provides many opportunities to orient people to the possibilities and potential of VR technology. Acclimating people to the site through such an app appeared to be a fun way to captivate an audiance.
